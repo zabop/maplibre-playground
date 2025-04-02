c3b943e64fbfc1f8bda15f5799bd757ce85140ed
zabop <szabopal96@gmail.com>
2025-04-02 22:55:57 +0200

    jump where you click

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import { Map } from "@vis.gl/react-maplibre";
 import "maplibre-gl/dist/maplibre-gl.css"; // See notes below
 
 function App() {
+  const [viewState, setViewState] = useState({
+    longitude: -100,
+    latitude: 40,
+    zoom: 18,
+  });
+
+  const handleClick = (event: MapEvent) => {
+    console.log(event);
+    const { lng, lat } = event.lngLat;
+    setViewState({
+      longitude: lng,
+      latitude: lat,
+      zoom: 18,
+    });
+  };
+
   return (
     <Map
-      initialViewState={{
-        longitude: 10,
-        latitude: 60,
-        zoom: 4.5,
-      }}
+      viewState={viewState}
       style={{ width: "100vw", height: "100vh" }}
       mapStyle="https://api.maptiler.com/maps/hybrid/style.json?key=iG31hHzqPDdOfM8MmYsP"
+      onClick={(event) => handleClick(event)}
     />
   );
 }
